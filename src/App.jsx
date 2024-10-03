a9ac608ab96f7fa173ca66e26fe34c50b85071b2
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-03 14:57:08 +0200

    hej

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
-import Header from "./components/Header/Header";
-import Stash from "./components/Stash/Stash";
-// import { Login } from "@leriksson/frk-1-components";
+// import Header from "./components/Header/Header";
+// import Stash from "./components/Stash/Stash";
+import { Login } from "@leriksson/frk-1-components";
+import styles from "./Test.module.css";
 
 function App() {
   return (
     <>
-      {/* <Login /> */}
-      <Header />
-      <Stash />
+      <Login style={{ color: "red" }} btnTitle="Sign in" />
+      {/* <Header /> */}
+      {/* <Stash /> */}
     </>
   );
 }
